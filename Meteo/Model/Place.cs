45414137df1e916344ce290324f6057fa0491ed2
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-12-15 12:24:48 +0200

    Add project files.

---
+++ Meteo/Model/Place.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meteo.Meteo.Model
+{
+    internal class Place
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string AdministrativeDivision { get; set; }
+        public string CountryCode { get; set; }
+
+    }
+}
---
+++ Meteo/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Meteo.Meteo.Model;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+var httpClient = new HttpClient();
+
+var httpResponse = await httpClient.GetAsync("https://api.meteo.lt/v1/places");
+
+// Get all places
+if (httpResponse.IsSuccessStatusCode)
+{
+    Console.WriteLine(httpResponse.StatusCode.ToString());
+
+    var contentString = await httpResponse.Content.ReadAsStringAsync();
+
+    var places = JsonConvert.DeserializeObject<List<Place>>(contentString);
+
+    var filteredPlaces = places.Where(p => p.Name == "Druskininkai");
+
+    foreach(var place in filteredPlaces)
+    {
+        Console.WriteLine($"Vietove: {place.Name}, kodas: {place.CountryCode} Rajonas: {place.AdministrativeDivision}, Salies kodas: {place.CountryCode}");
+    }
+  
+}
+
